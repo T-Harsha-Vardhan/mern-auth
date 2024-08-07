e9e0b0cbecd71e5d1744f8c4f55d92c8bf82848b
T-Harsha-Vardhan <harsharvardhantholeti17@gmail.com>
2024-08-07 09:24:06 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,9 @@ app.use("/api/users", userRouter);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => res.send("Server is ready"));
